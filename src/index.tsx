941ed7c3da87c7ed0b99979cda3c50be30a54c29
Rubén Giménez Martínez <rgimenez.etraid@grupoetra.com>
2025-11-04 17:42:07 +0100

    quitar código innecesario

--- src/index.tsx
+++ src/index.tsx
@@ -12,15 +12,6 @@ export function App() {
   );
 }
 
-function Resource(props) {
-  return (
-    <a href={props.href} target="_blank" class="resource">
-      <h2>{props.title}</h2>
-      <p>{props.description}</p>
-    </a>
-  );
-}
-
 if (typeof window !== "undefined") {
   hydrate(<App />, document.getElementById("app"));
 }
