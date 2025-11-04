e5524b8139fd020b20f0ecea9bec39e6d5a814d9
Rubén Giménez Martínez <rgimenez.etraid@grupoetra.com>
2025-11-04 18:01:01 +0100

    nueva version con signals

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,38 @@ import { hydrate, prerender as ssr } from "preact-iso";
 import preactLogo from "./assets/preact.svg";
 import "./style.css";
 
+import { signal } from "@preact/signals";
+
+const counter = signal(10);
+
 export function App() {
+  const increase = () => {
+    counter.value++;
+  };
+  const decrease = () => {
+    counter.value--;
+  };
+
   return (
     <div>
-      <h1>Hola mundo desde Preact!</h1>
-      <img src={preactLogo} class="logo" alt="Preact Logo" />
+      <div>
+        <h1>Hola mundo desde Preact!</h1>
+        <span>Contador: {counter.value}</span>
+      </div>
+      <div style={{ display: "flex", gap: "8px", justifyContent: "center" }}>
+        <button
+          style={{ width: "30px", fontSize: "1.5rem" }}
+          onClick={increase}
+        >
+          +
+        </button>
+        <button
+          style={{ width: "30px", fontSize: "1.5rem" }}
+          onClick={decrease}
+        >
+          -
+        </button>
+      </div>
     </div>
   );
 }
